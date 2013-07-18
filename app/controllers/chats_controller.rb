d4d3d0c7e02e4fcb3ce9a09c8fd317bf7392209f
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-07-18 15:27:13 -0400

    Username and avatar display next to chats

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -8,7 +8,7 @@ class ChatsController < ApplicationController
 
     data = {username: current_user.username,
                 avatar: current_user.image.thumb.url,
-                text: params[:chat_input]}
+                chat_text: params[:chat_input]}
 
     #Pusher.trigger(channel_name, event_name, data_to_send_with_event)
     Pusher.trigger(params[:channel], 'message', data)
