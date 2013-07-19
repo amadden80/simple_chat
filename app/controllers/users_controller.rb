5a7826311b36e9781f8ece801678564268d453dd
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-07-19 11:02:05 -0400

    Adding comments on controller and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,24 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.new(user_params) #uses the new strong parameters
+    if @user.save
+      session[:user_id] = @user.id #automatically logs user in after they sign up
+      redirect_to root_path
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  #New Rails 4 strong parameters
+  private
+  def user_params
+    #Requires the user hash to be inside of the params hash and to have the following attributes
+    params.require(:user).permit(:username, :password, :password_confirmation, :remote_image_url)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SimpleChatFour::Application.routes.draw do
   root to: 'chats#index'
 
+  resources :users, only: [:new, :create]
+
   post '/chats/send_chat', to: 'chats#send_chat', as: 'send_chat'
 
   get 'sessions/new' => 'sessions#new'
